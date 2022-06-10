7bc3c327949cae08939bac1d487786f7a1196eb1
Aryan <aryan.gholamlou@gmail.com>
2022-06-10 15:17:52 +0430

    solve maximum_subarray problem.

---
+++ easy_questions/maximum_subarray.py
@@ -0,0 +1,11 @@
+def maxSubArray(nums: list) -> int:
+    dp = []
+    dp.append(nums[0])
+    current_largest_sum = dp[0]
+    for i in range(1, len(nums)):
+        dp.append(max(dp[i-1] + nums[i], nums[i]))
+        if dp[i] > current_largest_sum:
+            current_largest_sum = dp[i]
+    return current_largest_sum        
+
+#print(maxSubArray())
